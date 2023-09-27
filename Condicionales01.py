15b5e0562125ee414f52e66c648a8522dd44a12a
Ivanna0407 <129338460+Ivanna0407@users.noreply.github.com>
2023-09-27 15:11:49 -0600

    Create Condicionales01.py

---
+++ Condicionales01.py
@@ -0,0 +1,7 @@
+n=int(input("Dame un número, el que quieras"))
+if(n>0):
+  print("Tu número es positivo")
+elif(n<0):
+  print("tu número es negativo")
+else:
+  print("tu número es 0 alias neutro")
